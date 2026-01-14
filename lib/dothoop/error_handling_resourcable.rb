2e5e55444ce74b0d5b08739ce62f1daf7efd3a30
Mwaki Harri Magotswi <magotswi@gmail.com>
2026-01-14 18:46:19 -0500

    adding a redirect error with info

--- lib/dothoop/error_handling_resourcable.rb
+++ lib/dothoop/error_handling_resourcable.rb
@@ -1,9 +1,13 @@
+require 'pry'
+
 module ErrorHandlingResourcable
   def self.included(base)
     base.send(:resources) do
       default_handler do |response|
         if (200...299).include?(response.status)
           next
+        elsif response.status == 301
+          raise Dothoop::RedirectError.new({status: response.status, body: response.body, redirect: response.headers[:location]})
         elsif response.status == 401
           raise Dothoop::UnauthorizedError.new("#{response.status}: #{response.body}")
         elsif response.status == 403
